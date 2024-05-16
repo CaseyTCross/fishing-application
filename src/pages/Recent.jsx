d1391a2a7ec0e7f6f6827ef4ea2cb8e3707bb480
Cross <caseycross1025@gmail.com>
2024-05-15 23:32:44 -0500

    update

--- src/pages/Recent.jsx
+++ src/pages/Recent.jsx
@@ -1,24 +1,8 @@
-import { Card } from "react-bootstrap";
-import { ListGroup } from "react-bootstrap";
-import fishImage from '../assets/fish1.jpg';
+
 
 function Recent() {
 
-    return (
-        <Card style={{maxWidth: '90%', textAlign: "center", marginTop: '10px'}}>
-            <Card.Img variant="top" src={fishImage} width={'30%'} />
-            <Card.Body>
-                <Card.Title>Northern Pike</Card.Title>
-                <Card.Text>Sucker in middle of channel</Card.Text>
-            </Card.Body>
-            <ListGroup className="list-group-flush">
-                <ListGroup.Item>Spinner | Primary/Secondary Color: Yellow/Yellow</ListGroup.Item>
-                <ListGroup.Item></ListGroup.Item>
-                <ListGroup.Item></ListGroup.Item>
-                <ListGroup.Item></ListGroup.Item>
-            </ListGroup>
-        </Card>
-    )
+    
 }
 
 export default Recent;
