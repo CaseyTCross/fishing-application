7639dc7d76ac5a5734ea6bae0e7d3d474a7c6dac
Cross <caseycross1025@gmail.com>
2024-05-16 14:43:11 -0500

    update

--- src/pages/Recent.jsx
+++ src/pages/Recent.jsx
@@ -7,7 +7,7 @@ function Recent() {
             <h1 className="recenth1">The Family Fishing App</h1>
 
             <p>"Professionally Feeding fish since 1994"</p>
-            <img src="src/assets/websitebackground.png" width={'50%'}/>
+            <img src="https://cross-fishing.nyc3.cdn.digitaloceanspaces.com/websitebackground.png" width={'50%'}/>
         </div>
         
     )
